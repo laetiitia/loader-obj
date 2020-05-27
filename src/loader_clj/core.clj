69963a46f7b0cf76ce7efd7c8cfe2f60b50f1a5f
laetiitia <phm.laetiti@gmail.com>
2020-05-27 20:59:52 +0200

    update version

--- src/loader_clj/core.clj
+++ src/loader_clj/core.clj
@@ -21,8 +21,8 @@
 ;---------------------------------------
 (defn- add-to-model
   "Add a value in the model"
-  ([model key val] 
-    (assoc model key (conj (get model key) val)))
+  ([model key val]
+   (assoc model key (conj (get model key) val)))
   ([model k1 k2 val]
    (assoc model k1 (assoc (get model k1) k2 val)))) ;;For the maps of vertices, normals, faces and text_coord
 
@@ -33,23 +33,23 @@
   [model x y z]
   (let [nb (count (get model :vertices))]
     (add-to-model model :vertices nb [(Float/parseFloat x)
-                                                          (Float/parseFloat y)
-                                                          (Float/parseFloat z)])))
+                                      (Float/parseFloat y)
+                                      (Float/parseFloat z)])))
 
 (defn- handle-vn
   "Add to the model the normal in normals"
   [model x y z]
   (let [nb (count (get model :normals))]
     (add-to-model model :normals nb [(Float/parseFloat x)
-                                                          (Float/parseFloat y)
-                                                          (Float/parseFloat z)])))
+                                     (Float/parseFloat y)
+                                     (Float/parseFloat z)])))
 
 (defn- handle-vt
   "Add to the model the texture coordinates in text_coord"
   [model u v]
   (let [nb (count (get model :text_coord))]
     (add-to-model model :text_coord nb [(Float/parseFloat u)
-                                                             (Float/parseFloat v)])))
+                                        (Float/parseFloat v)])))
 
 (defn- create-face
   "Create a list of (vertice text_cood normal)"
@@ -133,7 +133,7 @@
   (print "rbg : ")
   (println (get item :rbg))
   (print "vertice : ")
-  (p/pprint (into (sorted-map) (get item :vertices)))
+  (println (get item :vertices))
   (print "normals : ")
   (println (get item :normals))
   (print "faces : ")
@@ -159,14 +159,14 @@
       res)))
 
 (defn- addVt
-"Add coordinates texttures to the structure (maps)"
-[tc res vt keyw acc]
-(if (seq vt)
-  (if (contains? acc (first vt))
-    (addVt tc res (rest vt) keyw acc)
-    (addVt tc (assoc res keyw (into [] (concat (get res keyw) (get tc (first vt)))))
-           (rest vt) keyw (conj acc (first vt))))
-  [res acc]))
+  "Add coordinates texttures to the structure (maps)"
+  [tc res vt keyw acc]
+  (if (seq vt)
+    (if (contains? acc (first vt))
+      (addVt tc res (rest vt) keyw acc)
+      (addVt tc (assoc res keyw (into [] (concat (get res keyw) (get tc (first vt)))))
+             (rest vt) keyw (conj acc (first vt))))
+    [res acc]))
 
 
 (defn- getVn
@@ -174,29 +174,74 @@
   [f]
   (into [] (map #(first (rest (rest %))) f)))
 
+; Transform Faces and Textures
+;  * mapFaces : information about faces
+;  * textures : information about textures
+; Return couple of the new faces and textures
 (defn- transFnT
-"Transform :faces and :text_coord of the model into a mesh"
-([mapFaces textures] (transFnT textures (vals mapFaces) {} {} {}))
-([tc list res coord acc]
- (if (seq list)
-   (let [face (first list)
-         v (map #(dec %) (map #(first %) face)) ;Get nodes of a face
-         f (first (getVn face)) ;Get number of the face
-         vt (map #(first (rest %)) face)
-         [coord2 acc2] (addVt tc coord v f (get acc f #{}))]
-     (transFnT tc (rest list) (assoc res f (into [] (concat v (get res f)))) ;Add indices of a face
-               coord2 (assoc acc f acc2)))
-   [res coord])))
+  "Transform :faces and :text_coord of the model into a mesh"
+  ([mapFaces textures] (transFnT textures (vals mapFaces) {} {} {}))
+  ([tc list res coord acc]
+   (if (seq list)
+     (let [face (first list)
+           v (map #(dec %) (map #(first %) face)) ;Get nodes of a face
+           f (first (getVn face)) ;Get number of the face
+           vt (map #(first (rest %)) face)
+           [coord2 acc2] (addVt tc coord v f (get acc f #{}))]
+       (transFnT tc (rest list) (assoc res f (into [] (concat v (get res f)))) ;Add indices of a face
+                 coord2 (assoc acc f acc2)))
+     [res coord])))
+
+; Change a vertice according to it's face update
+;  * f : vector of vertice for one face
+;  * vs : list of all vertices 
+;  * n : number of vertices without duplicate
+; Return couple of the new vertices and faces
+(defn- modifVertices
+  [f vs n]
+  (loop [face f, resV [], resF [], acc {}, cpt n]
+    (if (seq face)
+      (let [ind (first face)]
+        (if (get acc (first face))
+          (recur (rest face) (vec (concat resV (get vs ind))) (conj resF (get acc ind)) acc cpt)
+          (recur (rest face) (vec (concat resV (get vs ind))) (conj resF cpt) (assoc acc ind cpt) (inc cpt))))
+      [resV, resF])))
+
+; Change a vertice according to it's face update
+;  * f : vector of vertice of all faces
+;  * vs : list of all vertices 
+; Return couple of the new vertices and faces
+(defn- transVertices
+  "Transforme Vertices and Face according to the update"
+  ([f vs]
+  (loop [size (map #(count (set %)) (vals (into (sorted-map) f))), k (keys (into (sorted-map) f)), resV [], resF {}, n 0]
+    (if (and (seq k) (seq size))
+      (let [[v face] (modifVertices (get f (first k)) vs n)]
+        (recur (rest size) (rest k) (conj resV v) (assoc resF (first k) face) (+ n (first size))))
+      [resV, resF]))))
+
+
+
+
+
+
+(if (get {1 "a"} 1)
+  :ok
+  :nil)
+
 
 (defn transformModel
   "Transform model of loader into a correct mesh"
   [model]
   (let [[f t] (transFnT (get model :faces) (get model :text_coord))
-        n (transNormal (get model :normals) (map #(count (set %)) (vals (into (sorted-map) f))))]
+        n (transNormal (get model :normals) (map #(count (set %)) (vals (into (sorted-map) f))))
+        [v f2] (transVertices f (get model :vertices))]
     (-> model
-        (assoc :faces f)
+        (assoc :faces f2)
+        (assoc :vertices v)
         (assoc :normals n)
         (assoc :text_coord t))))
+
 
 
 ;---------------------------------------
